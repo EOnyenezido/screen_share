2a94ef6fce2da132d2c6c4f54b5ca3a62b69a62e
EOnyenezido <sleonvictor@gmail.com>
2015-04-14 17:57:40 +0100

    x

---
+++ main.js
@@ -0,0 +1,30 @@
+navigator.getUserMedia = navigator.getUserMedia ||
+    navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+var constraints = {
+  audio: false,
+  video: {
+    mandatory: {
+        chromeMediaSource: 'screen',
+        maxWidth: 1280,
+        maxHeight: 720
+    },
+    optional: []
+  }
+};
+var video = document.querySelector('video');
+
+function successCallback(stream) {
+  window.stream = stream; // stream available to console
+  if (window.URL) {
+    video.src = window.URL.createObjectURL(stream);
+  } else {
+    video.src = stream;
+  }
+}
+
+function errorCallback(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+navigator.getUserMedia(constraints, successCallback, errorCallback);
